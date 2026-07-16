3fd14f463cdb1af80fb1b52829cad778f983f6af
abhayyadav06-del <abhay8127yadav@gmail.com>
2026-07-16 15:43:27 +0530

    Move requirements.txt to root

---
+++ house_price_prediction/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="House Price Prediction")
+
+st.title("🏠 House Price Prediction")
+
+st.write("Welcome to the House Price Prediction App")
+
+st.success("Deployment is working successfully!")
